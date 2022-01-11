5b876c2d633cd0df35380cba14b691566fde00b3
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2022-01-11 07:40:27 +0000

    Add files via upload

--- stella_teleop_bluetooth/src/stella_teleop_bluetooth.py
+++ stella_teleop_bluetooth/src/stella_teleop_bluetooth.py
@@ -16,7 +16,7 @@ if __name__=="__main__":
 
 	# Wait a second to let the port initialize
 	time.sleep(1)
-	
+
 	rospy.init_node('stella_teleop_bluetooth_node')
 	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 	rate = rospy.Rate(10)
@@ -24,13 +24,14 @@ if __name__=="__main__":
 	target_angular_vel  = 0.0
 
 	try:
-    		while True:
-        		if serial_port.inWaiting() > 0:
-            			data = serial_port.readline()
-            			a = data[0:5]
-            			a = str(a)
+		while True:
+			if serial_port.inWaiting() > 0:
+				data = serial_port.readline()
+				a = data[0:5]
+				a = str(a)
 				a = a.split("/")
-				
+				a[0] = a[0].replace("b'","")
+				a[1] = a[1].replace("'","")
 				if a[0] == 'F':
 					target_linear_vel = float(a[1]) * 1
 				elif a[0] == 'B':
@@ -42,13 +43,12 @@ if __name__=="__main__":
 				elif a[0] == 'S':
 					target_linear_vel  = 0.0
 					target_angular_vel = 0.0
-									
 			else:
 				target_linear_vel  = 0.0
-				target_angular_vel = 0.0		
-		
+				target_angular_vel = 0.0
+
 			twist = Twist()
-			
+
 			twist.linear.x = target_linear_vel
 			twist.linear.y = 0.0
 			twist.linear.z = 0.0
@@ -56,16 +56,15 @@ if __name__=="__main__":
 			twist.angular.x = 0.0
 			twist.angular.y = 0.0
 			twist.angular.z = target_angular_vel
-			
 
 			pub.publish(twist)
 			rate.sleep()
 
 	except KeyboardInterrupt:
-    		print("Exiting Program")
+		print("Exiting Program")
 	except Exception as exception_error:
-    		print("Error occurred. Exiting Program")
-    		print("Error: " + str(exception_error))
+		print("Error occurred. Exiting Program")
+		print("Error: " + str(exception_error))
 	finally:
-    		serial_port.close()
-    	pass
+		serial_port.close()
+	pass
